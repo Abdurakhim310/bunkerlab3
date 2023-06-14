69101c1a2db53744955486b64a5b521b2be844a6
Abdurakhim <brawlstarsaab310@gmail.com>
2023-06-14 14:12:53 +0500

    homework

---
+++ lesson-4/script.js
@@ -0,0 +1,169 @@
+//1
+// let nnumber = 0;
+// if (number>=0) number++;
+// console.log(number);
+
+//2
+// let nnumber = 0;
+// if (number>=0) number++;
+// else number-=2;
+// console.log(number);
+
+//3
+// let nnumber = 0;
+// if (number>0) number++;
+// else if(number<0) number-=2;
+// else number=10;
+// console.log(number);
+
+//4
+// let first = 1;
+// let second = 2;
+// let third = 3;
+// let result = 0;
+// if (first>=0) result++;
+// if (second >= 0) result++;
+// if (third >= 0) result++;
+// console.log(result);
+
+//5
+// let first = 1;
+// let second = 2;
+// let third = 3;
+// let musbat = 0;
+// let manfiy = 0;
+// if (first>=0) result++;
+// else manfiy++;
+// if (second >= 0) result++;
+// else manfiy++;
+// if (third >= 0) result++;
+// else manfiy++;
+// console.log(`musbat ${musbat}ta
+// manfiy ${manfiy}ta`);
+
+//6
+// let a = 1;
+// let b = 3;
+// if (a>b) console.log("a soni katta");
+// else if (a<b) console.log("b soni katta");
+// else console.log('sonlar teng');
+
+//7
+// let a = 1;
+// let b = 3;
+// if (a>b) console.log("ikkinchi son kichkina");
+// else if (a<b) console.log("birinchi son kichkina");
+// else console.log('sonlar teng');
+
+//8
+// let a = 1;
+// let b = 3;
+// if (a>b) console.log(a,b);
+// else if (a<b) console.log(b,a);
+// else console.log('sonlar teng');
+
+//9
+// let a = 1;
+// let b = 3;
+// if (b<a) b+=a;
+// console.log(`a = ${a}
+// b = ${b}`);
+
+//10
+// let a = 1;
+// let b = 3;
+// let katta;
+// if (a==b){
+//     a = 0;
+//     b =  0;
+// }else{
+//     a+=b;
+//     b+=a
+// }
+// console.log(`a = ${a}
+// b = ${b}`);
+
+//11
+// let a = 1;
+// let b = 3;
+// let katta;
+// if (a==b){
+//     a = 0;
+//     b =  0;
+// }else{
+//     if (a>b) b = a;
+//     else a = b;
+// }
+// console.log(`a = ${a}
+// b = ${b}`);
+
+//12
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// if (a<b && a<c) console.log(a);
+// else if (b < a && b < c) console.log(b);
+// else if (c < b && c < b) console.log(c);
+
+//13
+// let a = 11;
+// let b = 22;
+// let c = 33;
+// if ((a < b && a > c) || (a > b && a < c)) console.log(a);
+// else if ((b < a && b > c) || (b > a && b < c)) console.log(b);
+// else if ((c < b && c > a) || (c > b && c < a)) console.log(c);
+
+//14
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// if (a<b && a<c) console.log(a);
+// else if (b < a && b < c) console.log(b);
+// else if (c < b && c < b) console.log(c);
+// if (a > b && a > c) console.log(a);
+// else if (b > a && b > c) console.log(b);
+// else if (c > b && c > b) console.log(c);
+
+//15
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// if (a<b && a<c) console.log(b,c);
+// else if (b < a && b < c) console.log(a,c);
+// else if (c < b && c < b) console.log(a,b);
+
+
+                 
+
+
+
+//18
+// let a = 1;
+// let b = 2;
+// let c = 2;
+// if (c == b && c!=a)console.log('birinchi');
+// else if (b == a && b != c) console.log('uchinchi');
+// else if (a == c && a!= b) console.log('ikkinchi');
+
+//19
+// let a =1;
+// let b = 2;
+// let c = 2;
+// let d = 2;
+// if (d != a&& d != b&&d != c) console.log(d);
+// else if (c != a&&c != b  && c!=d) console.log(c);
+// else if (a!=b&&a!=c&&a!=d) console.log(a);
+// else if (b!=a&&b!=c&&b!=d) console.log(b);
+
+//20
+// let a =1;
+// let b = -190;
+// let c = -90;
+// let one = b-a;
+// let two = c-a;
+// if (one<0) one= one * -1;
+// if (two < 0) two = two * -1;
+// if (one<two)console.log(`eng yaqin nuqta = ${b}
+// orasidagi masofa = ${one}`);
+// else if (one > two) console.log(`eng yaqin nuqta = ${c}
+// orasidagi masofa = ${two}`);
