eb4e7bcf6b6cd55078695ad4ffa9f9b6e82bb13a
Abdurakhim <brawlstarsaab310@gmail.com>
2023-06-16 13:54:47 +0500

    for loop (homework)

---
+++ lesson-5/script.js
@@ -0,0 +1,139 @@
+//10
+// let s = 0;
+// let n = 10;
+// for(i = 0;i <= n;i++){
+//     let t = 1/i;
+//     s+=t;
+// }
+// console.log(s);
+
+//11
+// let n = 10;
+// let s = 0;
+// for (let i = 0;i<=n;i++){
+//     let t = (n + i) * (n + i);
+//     s+=t
+// }
+// console.log(s);
+
+//12
+// let s = 0;
+// let n = 5;
+// for (let i = 0; i<=n; i++){
+//     let t = 1+(i/10);
+//     s*=t
+// }
+// console.log(s);
+
+//13
+// let s = 0;
+// let n = 5;
+// let tf = true;
+// for (let i = 0; i<=n; i++){
+//     let t = 1+(i/10);
+//     if (tf ==  false){
+//         s-=t;
+//         tf = true
+//     }else{
+//         s+=t;
+//         tf = false
+//     }
+// }
+// console.log(s);
+
+//14
+// let s = 0;
+// let n = 5;
+// for (let i = 1; true; i+=2){
+//     let t = n*n;
+//     if (i == (t - 1)) break;
+//     s+=i;
+// }
+// console.log(s);
+
+//15
+// let a = 1;
+// let n = 5;
+// let res = 1;
+// for(let i = 1; i<=n;i++){
+//     res*=a;
+// }
+// console.log(res);
+
+
+
+//17
+// let n = 5;
+// let a = 2;
+// let res = 1;
+// for (let i = 0; i <= n;i++){
+//     let t = 1;
+//     for (let j = 1;j<=i;j++){
+//         t*=a;
+//     }
+//     res+=t;
+// }
+// console.log(res);
+
+
+//19
+// let n= 5;
+// let res = 1;
+// for(let i = 1;i<=n;i++){
+//     res*=i
+// }
+// console.log(res);
+
+//20
+// let n= 5;
+// let res = 0;
+// for(let i = 1;i<=n;i++){
+//     let t = 1;
+//     for (let j = 1;j<=i;j++){
+//         t*=j
+//     }
+//     res+=t
+// }
+// console.log(res);
+
+// 21
+// let n= 5;
+// let res = 0;
+// for(let i = 1;i<=n;i++){
+//     let t = 1;
+//     for (let j = 1;j<=i;j++){
+//         t*=j
+//     }
+//     res+=1/t
+// }
+// console.log(res);
+
+
+
+
+//36
+// let n = 2;
+// let k = 2;
+// let res = 0;
+// for(let i = 1;i<=n;i++){
+//     let t = 1;
+//     for (let j = 1;j<=k;j++){
+//         t*=i;
+//     }
+//     res+=t
+// }
+// console.log(res);
+
+// 37
+// let n = 2;
+// let k = 2;
+// let res = 0;
+// for(let i = 1;i<=n;i++){
+//     let t = 1;
+//     for (let j = 1;j<=k;j++){
+//         t*=i;
+//     }
+//     res+=t
+// }
+// console.log(res);
+
