ad7c384e965d8a5192c0f5ae8abbc307ee7f1044
Abdurakhim <brawlstarsaab310@gmail.com>
2023-06-20 20:31:53 +0500

    obj-1

---
+++ lesson-7/script.js
@@ -0,0 +1,41 @@
+// let restrangle = {
+//     width : 200,
+//     height : 300,
+// }
+// function getPerimeter(a,b){
+//     return 2*(a+b)
+// }
+// function getArea(a,b){
+//     return a*b
+// }
+// console.log(`P = ${getPerimeter(restrangle.width,restrangle.height)}
+// S = ${getArea(restrangle.width,restrangle.height)}`);
+
+
+
+
+
+
+
+// const PE = 3.14;
+// let circle = {
+//     radius : 3
+// }
+// function getCircumference(r){
+//     return PE*2*r
+// }
+// console.log(`Circumference = ${getCircumference(circle.radius)}`);
+
+
+
+
+
+
+let triangle = {
+    a: 10,
+    height: 15
+}
+function getArea2(a,b){
+    return a*b/2
+}
+console.log(`S = ${getArea2(triangle.a,triangle.height)}`);
